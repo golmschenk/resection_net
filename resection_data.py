f0b0311a0cfb172c1c548a4f2f6780e871fa6020
Greg Olmschenk <golmschenk@gmail.com>
2016-05-02 18:10:15 -0400

    Add docstrings

--- resection_data.py
+++ resection_data.py
@@ -13,15 +13,39 @@ class ResectionData(GoData):
     """
     @staticmethod
     def attain_pitch_from_gravity_vector(gravity_vector):
+        """
+        Determines the pitch angle given the normalized gravity vector.
+
+        :param gravity_vector: The normalized gravity vector.
+        :type gravity_vector: (int, int, int)
+        :return: The pitch angle.
+        :rtype: float
+        """
         z = gravity_vector[2]
         pitch = -(pi/2 - acos(z))
         return pitch
 
     @staticmethod
     def normalize_vector(vector):
+        """
+        Normalize a given vector.
+
+        :param vector: The vector to normalize.
+        :type vector: list[int]
+        :return: The normalized vector.
+        :rtype: list[int]
+        """
         return vector / np.linalg.norm(vector)
 
     def attain_roll_from_gravity_vector(self, gravity_vector):
+        """
+        Determines the roll angle given the normalized gravity vector.
+
+        :param gravity_vector: The normalized gravity vector.
+        :type gravity_vector: (int, int, int)
+        :return: The roll angle.
+        :rtype: float
+        """
         xy_vector = gravity_vector
         xy_vector[2] = 0
         xy_vector = self.normalize_vector(xy_vector)
