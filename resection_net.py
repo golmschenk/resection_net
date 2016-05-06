31d34c09829861678199c1d638fbb07d1da9b559
Greg Olmschenk <golmschenk@gmail.com>
2016-05-06 18:29:18 -0400

    Working linear classifier

--- resection_net.py
+++ resection_net.py
@@ -17,6 +17,50 @@ class ResectionNet(GoNet):
         super().__init__(*args, **kwargs)
 
         self.data = ResectionData()
+        self.step_summary_name = "Loss"
+        self.image_summary_on = False
+
+    def create_inference_op(self, images):
+        """
+        Performs a forward pass estimating label maps from RGB images.
+
+        :param images: The RGB images tensor.
+        :type images: tf.Tensor
+        :return: The label maps tensor.
+        :rtype: tf.Tensor
+        """
+        return self.create_linear_classifier_inference_op(images)
+
+    def create_linear_classifier_inference_op(self, images):
+        """
+        Performs a forward pass estimating label maps from RGB images using only a linear classifier.
+
+        :param images: The RGB images tensor.
+        :type images: tf.Tensor
+        :return: The label maps tensor.
+        :rtype: tf.Tensor
+        """
+        pixel_count = self.data.height * self.data.width
+        flat_images = tf.reshape(images, [-1, pixel_count * self.data.channels])
+        weights = weight_variable([pixel_count * self.data.channels, 2], stddev=0.001)
+        biases = bias_variable([2], constant=0.001)
+
+        flat_predicted_labels = tf.matmul(flat_images, weights) + biases
+        predicted_labels = tf.reshape(flat_predicted_labels, [-1, 2])
+        return predicted_labels
+
+    def create_loss_tensor(self, predicted_labels, labels):
+        """
+        Create the loss op and add it to the graph.
+
+        :param predicted_labels: The labels predicted by the graph.
+        :type predicted_labels: tf.Tensor
+        :param labels: The ground truth labels.
+        :type labels: tf.Tensor
+        :return: The loss tensor.
+        :rtype: tf.Tensor
+        """
+        return self.create_absolute_differences_tensor(predicted_labels, labels)
 
 
 if __name__ == '__main__':
